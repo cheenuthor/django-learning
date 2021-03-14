bdcabd243c376033b40ebe4942dab2da12b3a43c
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-14 15:39:57 +0530

    added page details post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -10,5 +10,7 @@ def posts(request):
     return render(request, 'blog/all-posts.html')
 
 
-def post_details(request):
-    pass
+def post_details(request,slug):
+    return render(request, 'blog/post-detail.html',{
+
+    })
