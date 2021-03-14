a5087c71ba0e9ae6563eb6b8fde68344aade7635
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-14 10:26:11 +0530

    added urls and views

---
+++ my_site/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,
+from . import views
+
+
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    # ?/posts/my-first-post(eg : slug)
+    path("post/<slug:slug>", views.post_details,
+         name="post-detail-page")
+
+]
