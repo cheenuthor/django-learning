3f941c5367ef794dc668a7554b591330c2b1a308
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-19 23:05:58 +0530

    change __str__ methods in model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -9,9 +9,18 @@ class Author(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(null=False)
 
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def __str__(self):
+        return self.full_name()
+
 
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
 
 
 class Post(models.Model):
@@ -22,5 +31,5 @@ class Post(models.Model):
     slug = models.SlugField(unique=True, default='', null=False)
     content = models.TextField(null=False, validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL,null=True, related_name='post')
+        Author, on_delete=models.SET_NULL, null=True, related_name='post')
     tag = models.ManyToManyField(Tag,)
