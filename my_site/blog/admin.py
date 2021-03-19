c0392996229c68f7401b1629a9ca1a0781abd211
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-19 23:19:02 +0530

    added slug in admin

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -7,6 +7,7 @@ from .models import Author, Tag, Post
 class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tag", "date")
     list_display = ("title", "date", "author")
+    prepopulated_fields = {"slug": ("title",)}
 
 
 admin.site.register(Author)
