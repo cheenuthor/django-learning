19e489d4f494d4d856c93004b86e0023bd8356f5
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-19 12:51:33 +0530

    configed admin Page

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.urls import reverse 
+from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
@@ -12,15 +12,17 @@ class Book(models.Model):
     author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
     # db_index makes performance better in querying , don't use it for all.
-    slug = models.SlugField(default="",blank=True,editable=False,null=False,db_index=True)
-    #black = True to set the value not required(ADMIN)
+    slug = models.SlugField(default="", blank=True,
+                             null=False, db_index=True)
+    # black = True to set the value not required(ADMIN)
     # editable = False to not display the property (ADMIN)
+
     def __str__(self):
         return f"{self.title} ({self.rating})"
-    
-    def save(self,*args, **kwargs):
+
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('book_detail',args=[self.slug])
+        return reverse('book_detail', args=[self.slug])
