048c105a9143f8e6e2231c6f9d2d2fb2c4b97b8f
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-23 14:21:27 +0530

    removed dummy post & added caption

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,78 +1,27 @@
-from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
-all_posts = [
-    {
-        "slug": "nature",
-        "image": "woods.jpg",
-        "author": "CHeeNU",
-        "date": date(2020, 7, 21),
-        "title": "Nature",
-        "excerpt": """There's noting like the views while hiking in the Mountain
-                    and I was'nt even prepared for what happend while I was enjoying
-                     the view""",
-        "content": """
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ab iste quia reiciendis nisi accusamus omnis debitis
-                    similique assumenda ex aliquam suscipit, ratione commodi
-                    sequi a cum rerum sint? Incidunt, natus!
-                   """
-    }, {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "CHeeNU",
-        "date": date(2020, 6, 21),
-        "title": "Hike in the mountains",
-        "excerpt": """There's noting like the views while hiking in the Mountain
-                    and I was'nt even prepared for what happend while I was enjoying
-                     the view""",
-        "content": """
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ab iste quia reiciendis nisi accusamus omnis debitis
-                    similique assumenda ex aliquam suscipit, ratione commodi
-                    sequi a cum rerum sint? Incidunt, natus!
-                   """
-    }, {
-        "slug": "programming",
-        "image": "coding.jpg",
-        "author": "CHeeNU",
-        "date": date(2020, 1, 21),
-        "title": "Programming is Great",
-        "excerpt": """There's noting like the views while hiking in the Mountain
-                    and I was'nt even prepared for what happend while I was enjoying
-                     the view""",
-        "content": """
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ab iste quia reiciendis nisi accusamus omnis debitis
-                    similique assumenda ex aliquam suscipit, ratione commodi
-                    sequi a cum rerum sint? Incidunt, natus!
-                   """
-    }
-]
 # Create your views here.
 
 
-def get_date(post):
-    return post['date']
-
-
 def starting_page(request,):
-    sorted_post = sorted(all_posts, key=get_date)
-    latest_post = sorted_post[-3:]
+    latest_post = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {
         "posts": latest_post
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
         "posts": all_posts
     })
 
 
 def post_details(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', {
-        "post": identified_post
+        "post": identified_post,
+        "tags":identified_post.tag.all()
     })
