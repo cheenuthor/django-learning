1491093db2529e5f2e7acc7dcb301c244b4c8b89
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-23 21:21:19 +0530

    added Comment Admin

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -10,7 +10,11 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "user_email", "post", "text")
+
+
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
